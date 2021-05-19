2bbc671e4e7d6dc60c81db09a6e60f01ea2fa866
Veegish <veegish.ramdani@proximitybbdo.fr>
2021-05-19 15:07:41 +0400

    Added POST, PUT and DELETE operations

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -40,5 +40,61 @@ namespace Catalog.Controllers
 
       return item.AsDto();
     }
+
+    // POST /items
+    [HttpPost]
+    public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
+    {
+      Item item = new()
+      {
+        Id = Guid.NewGuid(),
+        Name = itemDto.Name,
+        Price = itemDto.Price,
+        CreatedDate = DateTimeOffset.UtcNow
+      };
+      
+      repository.CreateItem(item);
+
+      return CreatedAtAction(nameof(GetItem), new { id = item.Id}, item.AsDto());
+    }
+
+    // PUT /item/{id}
+    [HttpPut("{id}")]
+    public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+    {
+      var existingItem = repository.GetItem(id);
+
+      if (existingItem is null) 
+      {
+        return NotFound();
+      }
+
+      // Item is immutable so we create a copy of it and initialize properties we want to modify in the copy
+      Item updatedItem = existingItem with 
+      {
+        Name = itemDto.Name,
+        Price = itemDto.Price
+      };
+
+      repository.UpdateItem(updatedItem);
+
+      return NoContent();
+    }
+
+    // DELETE /items{id}
+    [HttpDelete("{id}")]
+    public ActionResult DeleteItem(Guid id) 
+    {
+      var existingItem = repository.GetItem(id);
+
+      if (existingItem is null) 
+      {
+        return NotFound();
+      }
+
+      repository.DeleteItem(id);
+      
+      return NoContent();
+    }
   }
 }
