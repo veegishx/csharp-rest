d249b366318f8e3449866bdedf368a4395d1d621
Veegish <veegish.ramdani@proximitybbdo.fr>
2021-05-20 13:38:01 +0400

    Implemented GetItem method

--- Repositories/MongoDbItemsRepository.cs
+++ Repositories/MongoDbItemsRepository.cs
@@ -11,6 +11,7 @@ namespace Catalog.Repositories
     private const string databaseName = "catalog";
     private const string collectionName = "items";
     private readonly IMongoCollection<Item> itemsCollection;
+    private readonly FilterDefinitionBuilder<Item> filterBuilder = Builders<Item>.Filter;
 
     public MongoDbItemsRepository(IMongoClient mongoClient)
     {
@@ -30,7 +31,8 @@ namespace Catalog.Repositories
 
     public Item GetItem(Guid id)
     {
-      throw new NotImplementedException();
+      var filter = filterBuilder.Eq(item => item.Id, id);
+      return itemsCollection.Find(filter).SingleOrDefault();
     }
 
     public IEnumerable<Item> GetItems()
